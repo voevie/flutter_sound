6e51a8ea291b7d2e765df19420dff46b87d64b6f
zhang <zhangzhengpu@daoran.tv>
2019-09-03 17:27:23 +0800

    fix bug

--- android/src/main/java/com/dooboolab/fluttersound/FlutterSoundPlugin.java
+++ android/src/main/java/com/dooboolab/fluttersound/FlutterSoundPlugin.java
@@ -24,7 +24,6 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
-import
 
 /** FlutterSoundPlugin */
 public class FlutterSoundPlugin implements MethodCallHandler, PluginRegistry.RequestPermissionsResultListener, AudioInterface{ ;
